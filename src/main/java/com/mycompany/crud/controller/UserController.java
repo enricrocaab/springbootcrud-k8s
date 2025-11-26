57b6a7c519c80be75f4a6c1221ea132a0dd7b371
“enric” <enricrocaab@gmail.com>
2025-11-26 11:55:43 +0100

    Initial commit of springbootcrud-k8s

---
+++ src/main/java/com/mycompany/crud/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.mycompany.crud.controller;
+
+import com.mycompany.crud.model.User;
+import com.mycompany.crud.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RequestMapping("/users")
+
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users/list";
+    }
+
+   @GetMapping("/new")
+   public String showCreateForm(Model model) {
+       model.addAttribute("user", new User());
+       return "users/create";
+   }
+
+   @PostMapping
+   public String createUser(@ModelAttribute("user") User user) {
+       userRepository.save(user);
+       return "redirect:/users";
+   }
+
+   @GetMapping("/edit/{id}")
+   public String showEditForm(@PathVariable("id") Long id, Model model) {
+       model.addAttribute("user", userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id)));
+       return "users/edit";
+   }
+
+   @PostMapping("/update/{id}")
+   public String updateUser(@PathVariable("id") Long id, @ModelAttribute("user") User user) {
+       user.setId(id);
+       userRepository.save(user);
+       return "redirect:/users";
+   }
+
+   @GetMapping("/delete/{id}")
+   public String deleteUser(@PathVariable("id") Long id) {
+       userRepository.deleteById(id);
+       return "redirect:/users";
+   }
+}
+
---
+++ src/main/java/com/mycompany/crud/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mycompany.crud.repository;
+
+import com.mycompany.crud.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
+
